cc350b1ccc943468005d1b9a0a69a4a6a5d1519f
Matthew Fischer <mattfisc@gmail.com>
2019-11-16 02:26:56 -0800

    need plot

--- plot.py
+++ plot.py
@@ -1,23 +1,22 @@
-#import matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 import random
 import math
 import decimal
 
-# circle
-def circle(a):
+def distance(a):
+    first = a[0]
+    firstX = first[0]
+    firstY = first[1]
 
-    firstX = a[0]
-    firstY = a[1]
-
-    # radius center point
-    secondX = 0
-    secondY = 0
+    second = a[1]
+    secondX = second[0]
+    secondY = second[1]
 
     distance = math.sqrt((secondY - firstY)**2 + (secondX - secondY)**2)
 
     return distance
 
-# make tuple point
+
 def make_point():
     distance = 2
     while distance > 1:
@@ -25,14 +24,16 @@ def make_point():
         y = round(random.random()*2-1,2)
         
         point = (x,y)
-        distance = circle(point);
 
+        distance = distance(point);
+
+    
     return point
 
-# make list of tuple points
+
 def make_list_points():
     a = []
-    for i in range(1000):
+    for i in range(10):
         a.append(make_point())
     return a
 
@@ -43,3 +44,14 @@ print(a)
 
 
 # -- OUTPUT
+#[(-0.31, -0.44), (0.32, 0.92), (0.97, -0.14), (-0.95, -0.71), (-0.72, -0.45), (-0.83, 0.51), (-0.06, 0.51), (-0.62, 0.58), (0.75, -0.27), (-0.88, 0.71)]
+#distance 1.486472334084964
+#(-0.31, -0.44) (0.32, 0.92)
+#distance 1.1498260738042079
+#(-0.31, -0.44) (0.97, -0.14)
+#distance 0.3612478373637688
+#(-0.31, -0.44) (-0.95, -0.71)
+#distance 0.2701851217221259
+#(-0.31, -0.44) (-0.72, -0.45)
+
+#closest pair: ((-0.31, -0.44), (-0.72, -0.45))
