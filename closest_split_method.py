f2b9a25d988870c89b9f0e4181caea8e6a9d6c69
Matthew Fischer <mattfisc@gmail.com>
2019-11-18 12:14:43 -0800

    tuple sort x y

--- closest_split_method.py
+++ closest_split_method.py
@@ -1,14 +1,26 @@
 import math
 import random
-
+from operator import itemgetter
+def sortY(tup):  
+      
+    # getting length of list of tuples 
+    lst = len(tup)  
+    for i in range(0, lst):  
+          
+        for j in range(0, lst-i-1):  
+            if (tup[j][1] > tup[j + 1][1]):  
+                temp = tup[j]  
+                tup[j]= tup[j + 1]  
+                tup[j + 1]= temp  
+    return tup  
 
 def make_point():
     
     # make tuple of(x,y)
    
         # tuples of -10 to 10
-    x = round(random.random()*11-1,2)
-    y = round(random.random()*11-1,2)
+    x = round(random.randrange(-10,10),2)
+    y = round(random.randrange(-10,10),2)
     
     point = (x,y)
 
@@ -31,9 +43,17 @@ def split_pair(x,y):
     
     delta = 1.1
     
-    
+
 
 
 a = make_list_points()
-print(a)
+print("Non sorted list: \n",a)
+
+# sort x axis
+a.sort()
+print("x axis sort:\n",a)
+
+sortY(a)
+print("y axis sort:\n",a)
+
 
