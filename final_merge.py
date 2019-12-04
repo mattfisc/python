b985ce728c21e8e1f4843cf352e2b4140433b3a9
Matthew Fischer <mattfisc@gmail.com>
2019-12-04 11:38:26 -0800

    merge

--- final_merge.py
+++ final_merge.py
@@ -1,3 +1,8 @@
+import random
+import time
+
+level = 0
+
 def mergeSort(a):
     if len(a) == 1:
         return a
@@ -6,4 +11,19 @@ def mergeSort(a):
         frontEnd = mergeSort(a[:len(a)//2])       
         backEnd = mergeSort(a[len(a)//2:])
         return merge2(frontEnd,backEnd)
-    
+
+def RSelect(arr,l,r,k):
+    global level
+    level = 0
+    return arr
+
+
+
+
+#test
+n=1000
+a=[]
+for i in range(n):
+    a.append.(i)
+t1= time.time()
+RSelect(a,0,n-1,n//2)
