c419a6abb78409dd1b389f353e6bbdaebadb4531
Matthew Fischer <mattfisc@gmail.com>
2019-12-03 16:47:53 -0800

    python assignment 18 almost done

--- HW18_closest_split_method.py
+++ HW18_closest_split_method.py
@@ -1,8 +1,7 @@
 import math
 import random
-from operator import itemgetter
 
-
+# sort by y point
 def sortY(tup):  
       
     # getting length of list of tuples 
@@ -21,8 +20,8 @@ def make_point():
     # make tuple of(x,y)
    
         # tuples of -10 to 10
-    x = round(random.uniform(-10,10),2)
-    y = round(random.uniform(-10,10),2)
+    x = round(random.uniform(0,10),2)
+    y = round(random.uniform(0,10),2)
     
     point = (x,y)
 
@@ -40,38 +39,94 @@ def make_list_points():
 
 
 
-# find closest from delta
-def closest_point(a):
-    # sort x
-    a.sort()
-    #print("sorted x\n", a)
 
+def distance(first,second):
 
-    # find middle
-    # right side keeps on the line
-    middleI = 0
-    for i in range(len(a)):
-        if a[i][0] < 0:
-            print(a[i][0])
-            middleI = i
-        else:
-            break
+    # max distance rough
+    delta = 30
+    closestPair = []
 
-    firstX=a[:middleI+1]
-    print("\nleft side\n",firstX)
-    secondX=a[middleI+1:]
-    print("\nright side\n",secondX)
-    return firstX,secondX
+    for i in range(len(a)-1):
+        for j in range(i+1,len(a)):
+
+            firstX = first[0]
+            firstY = first[1]
+
+            secondX = second[0]
+            secondY = second[1]
+
+            check = math.sqrt((secondY - firstY)**2 + (secondX - secondY)**2)
+
+            if check < delta:
+                # new closest pair
+                closestPair = [] # erase old
+
+                # add new pair
+                closestPair.append(first) 
+                closestPair.append(second)
+                delta = check
+
+                print("distance",delta)
+    return delta
+def closestPair(px1,px2,py1,py2):
     
-def closestPair(lx,ly,rx,ry):
-    if lx < 3 and 
+    lx = px1[:len(px1)//2]
+    rx = px1[len(px2)//2:]
+    ly = py1[:len(py1)//2]
+    ry = py2[len(py1)//2]
+
+    #median is the largest left side
+    median = lx[-1]
+
+    #base case if less than 3
+    if lx < 3:
+        return lx[0],lx[1]
+    elif ly < 3:
+        return ly[0], ly[1]
+    elif rx < 3:
+        return rx[0],rx[1]
+    elif ry < 3:
+        return ry[0],ry[1]
+
+    # all other cases
+    else:
+        bestL = closestPair(lx,ly)
+        bestR = closestPair(rx,ry)
+        bestS = closestSplitPair(px,py)
+        
+def closestSplitPair(lx,ly,rx,ry):
+    sy = ly + ry
+    q = sortY(sy)
+
+    # worst case senario
+    best = 30
+    bestPair = []
+    for i in range(len(sy)-1):
+        for j in range(7):
+
+            # check distance
+            d = distance(q[i],q[i+j])
+
+            if d < best:
+                best = d
+                print("best split list",best)
+                bestPair = []
+                bestPair.append(q[i])
+                bestPair.append(q[i+j])
+    return bestPair
 
 
-
+# make list of points
 a = make_list_points()
 
-closest = closest_point(a)
+# sort list by x
+px = a.sort
 
+# sort list by y
+py = sortY(a)
+
+# find closest point
+best = closestPair(px,px,py,py)
 
 #print("Non sorted list: \n",a)
 
