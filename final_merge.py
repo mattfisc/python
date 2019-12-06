22d9615596cc836b1bacfe53a6416950727fda65
Matthew Fischer <mattfisc@gmail.com>
2019-12-05 18:40:37 -0800

    final

--- final_merge.py
+++ final_merge.py
@@ -1,29 +1,33 @@
+
 import random
-import time
 
-level = 0
+def partition(A, l, r):
+    pivot = A[l]  # the pivot value
+    i = l+1       # We assume the pivot has been moved to the front of the
+    # array-This is done in the quicksort method
 
-def mergeSort(a):
-    if len(a) == 1:
-        return a
-    else:
+    for j in range(l+1, r):
+        if A[j] < pivot:
+            A[j], A[i] = A[i], A[j]
+            i += 1
+    A[l], A[i-1] = A[i-1], A[l]
 
-        frontEnd = mergeSort(a[:len(a)//2])       
-        backEnd = mergeSort(a[len(a)//2:])
-        return merge2(frontEnd,backEnd)
+    return i-1
 
-def RSelect(arr,l,r,k):
-    global level
-    level = 0
-    return arr
+def quicksort(A, l, r):
+    if l >= r:
+        return
 
+    i = choose_pivot(A, l, r)  # Chooses a pivot randomly from l to r
+    # Moves the random pivot to the beginning of the array
+    A[l], A[i] = A[i], A[l]
+    j = partition(A, l, r)   # Then do the paretitioning
 
+    quicksort(A, l, j)      # 2 recursive calls- this one from l to j-1
+    quicksort(A, j+1, r)    # This one from j+1 to the end of the array
 
+def choose_pivot(A, l, r):
+    # Randomly pick a value from (l , r)
+    rpivot = int((r-l)*random.random()) + l
+    return rpivot                        # This is our random pivot
 
-#test
-n=1000
-a=[]
-for i in range(n):
-    a.append(i)
-t1= time.time()
-RSelect(a,0,n-1,n//2)
