b5cdd33fd30cc1d576d2c4659f2bfa4c06408b5e
Matthew Fischer <mattfisc@gmail.com>
2019-12-06 01:08:53 -0800

    python

--- final_merge.py
+++ final_merge.py
@@ -9,16 +9,16 @@ def partition(A, l, r):
     pivot = A[l]  # the pivot value
     i = l+1       # We assume the pivot has been moved to the front of the
     # array-This is done in the quicksort method
-    print("A before: ",A)
+    
     parTime = time.time()
     for j in range(l+1, r):
         if A[j] < pivot:
             A[j], A[i] = A[i], A[j]
             i += 1
     A[l], A[i-1] = A[i-1], A[l]
-    print("Partition time for array of length", len(A))
+    
     print(time.time()-parTime)
-    print("A after: ",A)
+    
 
     return i-1
 
@@ -27,7 +27,7 @@ def quicksort(A, l, r):
         return
     global count
     count += 1
-    print("count: ",count)
+    
     i = random_pivot(A, l, r)  # Chooses a pivot randomly from l to r
 
     # move pivot to the front of array
@@ -39,25 +39,26 @@ def quicksort(A, l, r):
     quicksort(A, j+1, r)    # This one from j+1 to the end of the array
 
 
-    return A
+    
 
 
 def random_pivot(A, l, r):
     # Randomly pick a value from (l , r)
     rpivot = int((r-l)*random.random()) + l
-    print("random index: ",rpivot)
     return rpivot                        
 
 # make random arr
 def make_array():
     arr = []
-    for i in range(10):
+    for i in range(10000):
         arr.append(random.randint(0,100))
     return arr
 
 
 a = make_array()
-print("this is the array: ",a)
+
 st = time.time()
 print(quicksort(a,0,len(a)))
+print("Array length", len(a))
 print(time.time()-st)
+print("count: ",count)
